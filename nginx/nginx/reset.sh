ea52228373762b2467d64247321924adfb4b9a5d
Steven Chien <steven-chien@users.noreply.github.com>
2025-12-25 12:53:14 +0000

    Update reset.sh

--- nginx/nginx/reset.sh
+++ nginx/nginx/reset.sh
@@ -2,7 +2,7 @@
 #IFNAME=enp23s0f0np0
 IFNAME=enp8s0f0np0
 # source inside build folder
-./ebpf_redirect_block_load.sh
+./ebpf_redirect_block_load.sh $IFNAME
 tc filter del dev "$IFNAME" parent 1:
 #tc filter del dev ens1f0np0 ingress
 #tc filter del dev ens1f0np0 egress
