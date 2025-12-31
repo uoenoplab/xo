08907b7010185b19a913f0a8f5df6e8ffff46519
Steven Chien <steven-chien@users.noreply.github.com>
2025-12-31 13:30:31 +0000

    Update reset.sh

--- nginx/nginx/reset.sh
+++ nginx/nginx/reset.sh
@@ -1,6 +1,13 @@
-#IFNAME=ens1f0np0
-#IFNAME=enp23s0f0np0
+#!/bin/bash
+
 IFNAME=enp8s0f0np0
+if [[ $# -eq 0 ]] ; then
+	echo 'Provide interface name.'
+	exit 0
+fi
+
+IFNAME="$1"
+
 # source inside build folder
 ./ebpf_redirect_block_load.sh $IFNAME
 tc filter del dev "$IFNAME" parent 1:
